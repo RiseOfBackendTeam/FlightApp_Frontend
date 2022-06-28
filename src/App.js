42479c4be5ea52bfb362a9184debb8cdf60ccb86
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-06-28 17:19:33 +0300

    Router done with private func

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import PublicRouter from "./router/PublicRouter";
 
 function App() {
   return (
     <div className="App">
-
+      <PublicRouter />
     </div>
   );
 }
